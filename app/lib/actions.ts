1b4cbcd8e99c327c59661f598038c9eab0c461d2
sueasen <suehara.akihiko@gmail.com>
2024-06-24 10:59:24 +0900

    update

---
+++ app/lib/actions.ts
@@ -0,0 +1,79 @@
+'use server';
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ date: true });
+
+export async function createInvoice(formData: FormData) {
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
+  const rawFormData2 = Object.fromEntries(formData.entries());
+
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+
+  // Test it out:
+  console.log(rawFormData);
+  console.log(rawFormData2);
+}
+
+export async function updateInvoice(formData: FormData) {
+  console.log('aaaaaa');
+  console.log(formData);
+  // フォームから入力情報取得 id, customerId, amount, status
+  const { id, customerId, amount, status } = UpdateInvoice.parse({
+    id: formData.get('id'),
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  // 金額 amount の変換処理
+  const amountInCents = amount * 100;
+
+  // 更新SQL実行 ( UPDATE テーブル名 SET 更新カラム.. WHERE 条件 )
+  await sql`
+    UPDATE invoices SET 
+       customer_id = ${customerId}
+       , amount = ${amountInCents}
+       , status = ${status}
+    WHERE
+      id = ${id}
+  `;
+
+  // DB再取得(キャッシュクリア), リダイレクト
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,24 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((text: string) => {
+    console.log(text);
+
+    const params = new URLSearchParams(searchParams);
+    params.delete('page');
+    // text があれば query に設定, 無ければ初期化
+    text ? params.set('query', text) : params.delete('query');
+    replace(`${pathname}?${params.toString()}`);
+  }, 500);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +27,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
